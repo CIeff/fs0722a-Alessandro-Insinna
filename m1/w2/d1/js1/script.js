ca86af04e7462a8ef49fa17bf5bcbf569f5af197
CIeff <a.insinna97@gmail.com>
2022-10-30 17:06:48 +0100

    Progetto settimanale.02

---
+++ m1/w2/d1/js1/script.js
@@ -0,0 +1,27 @@
+/*
+Questo èm un commento su più righe
+*/
+
+//commento su una riga (andando a capo finisce)
+
+/****************************************************************************
+ * Direttive di output
+ */
+
+//alert('mostra questa stringa')
+
+console.log('Mostra questa scritta');
+
+console.log('3 + 2');
+
+console.log('%c Mostra questa stringa','color:red');
+
+document.getElementById('test').innerHTML ='<b>Scrivo dentro l-elemento</b> preso e posso anche usare del css cancellandone il contenuto';
+
+// entra in atto dopo il caricamento dello script js  document.write('Ciao')
+
+
+
+
+
+
