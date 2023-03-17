cf4960aaba04b57aafdd23e77d9f7683ef0ba836
CIeff <a.insinna97@gmail.com>
2023-03-17 16:25:32 +0100

    progetto settimanale

---
+++ eclipse/rilevamentoincendi/src/main/java/com/rilevamentoincendi/rilevamentoincendi/RilevamentoincendiApplication.java
@@ -0,0 +1,13 @@
+package com.rilevamentoincendi.rilevamentoincendi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RilevamentoincendiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RilevamentoincendiApplication.class, args);
+	}
+
+}
---
+++ eclipse/rilevamentoincendi/src/test/java/com/rilevamentoincendi/rilevamentoincendi/RilevamentoincendiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rilevamentoincendi.rilevamentoincendi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RilevamentoincendiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
